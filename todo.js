9a6e1238cd633e30deebe9fa5d05e8befb23c14f
NewsCred-Nazmul <newscred@NewsCred-air-16.local>
2014-05-26 19:46:37 +0600

    filter, directives added

--- todo.js
+++ todo.js
@@ -1,16 +1,47 @@
-function TodoController($scope) {
+var todoAppThirdParty = angular.module('todoApp.thirdParty', ['truncate']);
+var todoAppFilters = angular.module('todoApp.filters', []);
+var todoAppDirectives = angular.module('todoApp.directives', []);
+
+var todoApp = angular.module('todoApp', ['todoApp.thirdParty', 'todoApp.filters', 'todoApp.directives']);
+
+todoAppFilters.filter('PrettyFy', function () {
+	return function(input, brace) {
+		if (!input) { return ''; }
+		if (typeof input !== 'string') { return input; }
+		if (brace) {
+			input = '<<' + input + '>>';
+		} else {
+			input = 'OXOX' + input + 'XOXO';			
+		}
+    return input;
+  };
+});
+
+todoAppDirectives.directive('comment', function() {
+  return {
+  	restrict: 'AE',
+  	replace: true,
+  	scope: {element: '=element'},
+  	templateUrl: 'dfm.html'
+  };
+});
+
+todoApp.controller('TodoController', function ($scope) {
 	$scope.name = 'NewsCred';
 
 	$scope.todos = [];
 
 	$scope.add = function ($event) {
 		if ($event.keyCode === 13 && $scope.name.trim()) {
-			$scope.todos.push(
-				{ name: $scope.name }
-			);
+			$scope.todos = _addToTodos($scope.todos, $scope.name);
 			$scope.name = '';
 		}
 	};
+
+	function _addToTodos(todos, name) {
+		todos.push({ name: name });
+		return todos;
+	}
 
 	$scope.remove = function ($index) {
 		$scope.todos.splice($index, 1);
@@ -29,4 +60,4 @@ function TodoController($scope) {
 			todo.name = todo.tempName;
 		}
 	};
-}
+});
